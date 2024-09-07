5ae51437ffe2bb71ddcba1f44a8a2d33184c71ab
Sachin Ramesh Tidke <sachintidke551@gmail.com>
2024-09-07 13:38:36 +0530

    Add files via upload

---
+++ space_complexity.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,add;
+    printf("enter the value n1:-");
+    scanf("%dd",&n1);
+    printf("enter the value n2:-");
+    scanf("%dd",&n2);
+
+    add=n1+n2;
+    printf("addition =%d",add);
+
+    return 0;   
+
+}
